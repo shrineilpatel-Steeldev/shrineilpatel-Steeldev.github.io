38980aff35e5ea9e617c4aacda98cbfd5242b28a
shrineilpatel-Steeldev <shrineil.patel@gmail.com>
2025-12-18 16:03:36 -0600

    Update hero.js
    changed title to black steel online

--- components/hero.js
+++ components/hero.js
@@ -11,7 +11,7 @@ class CustomHero extends HTMLElement {
                 <img src="http://static.photos/technology/1200x630/1" alt="Hero Banner" class="w-full hero-image h-64 object-cover">
                 <div class="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-gray-900 to-transparent h-32"></div>
                 <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-                    <h1 class="text-3xl md:text-4xl font-bold mb-4">Welcome to Amazon Clone</h1>
+                    <h1 class="text-3xl md:text-4xl font-bold mb-4">Welcome to Black Steel Online</h1>
                     <p class="text-lg mb-6">Shop millions of products with fast delivery</p>
                     <a href="#" class="bg-yellow-400 hover:bg-yellow-500 text-gray-900 font-bold py-2 px-6 rounded-lg inline-block">Shop Now</a>
                 </div>
